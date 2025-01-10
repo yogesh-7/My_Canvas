88734da3cb71c2b51dcfcedd839eb52b72553994
Yogesh Bansal <54579951+yogesh-7@users.noreply.github.com>
2025-01-10 19:39:40 +0530

    UI is ready

--- app/src/main/java/com/helloyogesh/mycanvas/MainActivity.kt
+++ app/src/main/java/com/helloyogesh/mycanvas/MainActivity.kt
@@ -3,84 +3,13 @@ package com.helloyogesh.mycanvas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyCanvas()
+            PathDemonstration()
         }
     }
 }
 
-@Composable
-fun MyCanvas() {
-    Canvas(
-        modifier = Modifier
-            .padding(20.dp)
-            .size(300.dp)
-    ) {
-        drawRect(
-            color = Color.Black,
-            size = size
-        )
-        drawRect(
-            color = Color.Red,
-            topLeft = Offset(100f, 100f),
-            size = Size(100f, 100f),
-            style = Stroke(
-                width = 3.dp.toPx()
-            )
-        )
-        drawCircle(
-            brush = Brush.radialGradient(
-                colors = listOf(Color.Green,Color.Red, Color.Yellow),
-                center = center,
-                radius = 100f
-            ),
-            radius = 100f,
-        )
-        drawArc(
-            color = Color.Green,
-            startAngle = 0f,
-            sweepAngle = 270f,
-            useCenter = false,
-            topLeft = Offset(100f, 500f),
-            size = Size(200f, 200f),
-            style = Stroke(
-                width = 3.dp.toPx()
-            )
-        )
-        drawOval(
-            color = Color.Magenta,
-            topLeft = Offset(500f, 100f),
-            size = Size(200f, 300f)
-        )
-        drawLine(
-            color = Color.Cyan,
-            start = Offset(300f, 700f),
-            end = Offset(700f, 700f),
-            strokeWidth = 5.dp.toPx()
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MyCanvasPreview() {
-    MyCanvas()
-}
