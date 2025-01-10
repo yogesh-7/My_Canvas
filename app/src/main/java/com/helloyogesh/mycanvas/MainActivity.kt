351a3329362cbb2dd39da9c46442b2ed0fb46c00
Yogesh Bansal <54579951+yogesh-7@users.noreply.github.com>
2025-01-10 20:14:08 +0530

    UI is ready

--- app/src/main/java/com/helloyogesh/mycanvas/MainActivity.kt
+++ app/src/main/java/com/helloyogesh/mycanvas/MainActivity.kt
@@ -3,11 +3,16 @@ package com.helloyogesh.mycanvas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -18,24 +23,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Canvas(modifier = Modifier.fillMaxSize()) {
-                val path = Path().apply {
-                    moveTo(2000f, 100f)
-                    lineTo(100f, 500f)
-                    lineTo(500f, 500f)
-//                    quadraticBezierTo(800f, 300f, 500f, 100f)
-                    cubicTo(800f, 500f, 800f, 100f, 500f, 100f)
-                }
-                drawPath(
-                    path = path,
-                    color = Color.Red,
-                    style = Stroke(
-                        width = 10.dp.toPx(),
-                        cap = StrokeCap.Round,
-                        join = StrokeJoin.Miter,
-                        miter = 0f
+            val pathPortion = remember {
+                Animatable(initialValue = 0f)
+            }
+            LaunchedEffect(key1 = true) {
+                pathPortion.animateTo(
+                    targetValue = 1f,
+                    animationSpec = tween(
+                        durationMillis = 2500
                     )
+                )
+            }
+            val path = Path().apply {
+                moveTo(100f, 100f)
+                quadraticBezierTo(400f, 400f, 100f, 400f)
+            }
+            val outPath = Path()
+            PathMeasure().apply {
+                setPath(path, false)
+                getSegment(0f, pathPortion.value * length, outPath, true)
+            }
 
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawPath(
+                    path = outPath,
+                    color = Color.Red,
+                    style = Stroke(width = 5.dp.toPx(), cap = StrokeCap.Round)
                 )
             }
         }
